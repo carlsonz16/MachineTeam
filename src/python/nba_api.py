31c97812208ebd06868eb52ee2791e4a9307cb6f
jakedamico <77427769+jakedamico@users.noreply.github.com>
2024-04-14 11:19:38 -0400

    Delete src directory

--- src/python/nba_api.py
+++
@@ -1,6 +0,0 @@
-from nba_api.stats.endpoints import playercareerstats
-
-# Nikola Jokić
-career = playercareerstats.PlayerCareerStats(player_id='203999')
-print(career)
-print(career.get_dict())
--- src/python/sportsipy.py
+++
@@ -1,5 +0,0 @@
-from sportsipy.nba.teams import Teams
-
-teams = Teams()
-for team in teams:
-    print(team.name, team.abbreviation)
